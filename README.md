7d5cc51245247b2c63bc7d0c253cdb029005aef5
andy <andy@7shangzuo.com>
2018-06-02 16:53:37 +0800

    新增

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+--  中文社区
+
+    https://www.oschina.net/question/tag/php-yaf
+    
 --  官方文档
  
     http://www.laruence.com/manual/yaf.config.html
@@ -6,19 +10,32 @@
 
 -- php.ini 添加配置 配合 application.ini 使用
 
-
+    # php.ini
 	[yaf]
 	extension=php_yaf.dll
 	yaf.environ=development
-
+    
+    # application.ini
+    [development]
+    application.directory=APP_PATH "/application/"
+    
 
 -- 类的加载规则
 
+    1. 本地类
+    
+    2. 全局类
+    
 
 -- 如何获取和配置$_ENV  
 
-    https://blog.csdn.net/zhezhebie/article/details/72765262
+    
 
+[参考博客](https://blog.csdn.net/zhezhebie/article/details/72765262)
 
     1. getenv('PATHEXT')
     2. $_ENV
+    
+
+
+[yaf 集成各种类 开源框架](https://github.com/sillydong/CZD_Yaf_Extension)
--- public/index.php
+++ public/index.php
@@ -1,20 +1,35 @@
 <?php  
 
-	// echo __FILE__; // 文件所在目录，包含文件名 D:\wamp64\www\web.7yaf.com\public\index.php
-	// echo dirname(__FILE__); // 文件夹路径 D:\wamp64\www\web.7yaf.com\public
-	// echo realpath(dirname(__FILE__) . '/../'); // D:\wamp64\www\web.7yaf.com
-	define("APP_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
-	
-	// error_reporting(0);
+// echo __FILE__; // 文件所在目录，包含文件名 D:\wamp64\www\web.7yaf.com\public\index.php
+// echo dirname(__FILE__); // 文件夹路径 D:\wamp64\www\web.7yaf.com\public
+// echo realpath(dirname(__FILE__)); die; D:\wamp64\www\web.7yaf.com\public
+// echo realpath(dirname(__FILE__) . '/../'); // D:\wamp64\www\web.7yaf.com
 
-	$app  = new Yaf_Application(APP_PATH . "/conf/application.ini");  
+/*define("APP_ROOT",  realpath(dirname(__FILE__) . '/../'));
+define('APP_PATH', APP_ROOT . '/application');
+define('APP_CONFIG', APP_ROOT . '/conf');
 
-    /*var_dump($_ENV);
-    $e = getenv('PATHEXT');
-    var_dump($e);*/
+// 实例化Bootstrap，一次调用Bootstrap中所有 _init开头的方法
+$app  = new Yaf_Application(APP_CONFIG . "/application.ini");*/
 
-	// $app->run();  
-  	$app->bootstrap() // 可选
-        ->run();
+define("APP_PATH",  realpath(dirname(__FILE__) . '/../'));
+
+//$app  = new Yaf_Application(APP_PATH . "/conf/application.ini");
+
+// 第二个参数用来确认不同环境的配置 对应config中的内容
+$app  = new Yaf_Application(APP_PATH . "/conf/application.ini", "common");
+
+
+// error_reporting(0);
+//定义全局 library
+ini_set("yaf.library", APP_PATH . "/library");
+
+/*var_dump($_ENV);
+$e = getenv('PATHEXT');
+var_dump($e);*/
+
+// $app->run();
+$app->bootstrap() // 可选
+    ->run();
 
 
